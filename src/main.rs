b68e684ef3f4a816ea2932c3aca6f9e5258140e5
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2022-07-27 22:35:10 +0200

    first structure

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+use bdk::bitcoin::Network;
+use bdk::database::MemoryDatabase;
+use bdk::keys::{DerivableKey, GeneratableKey, GeneratedKey, ExtendedKey, bip39::{Mnemonic, WordCount, Language}};
+use bdk::template::Bip84;
+use bdk::{miniscript, Wallet, KeychainKind};
+
+use embedded_graphics::{
+    mono_font::MonoTextStyleBuilder,
+    prelude::*,
+    primitives::{Circle, Line, PrimitiveStyle},
+    text::{Baseline, Text, TextStyleBuilder},
+};
+use embedded_hal::prelude::*;
+use epd_waveshare::{
+    color::*,
+    epd2in13_v2::{Display2in13, Epd2in13},
+    graphics::DisplayRotation,
+    prelude::*,
+};
+use linux_embedded_hal::{
+    spidev::{self, SpidevOptions},
+    sysfs_gpio::Direction,
+    Delay, Pin, Spidev,
+};
+
+fn draw_text(display: &mut Display2in13, text: &str, x: i32, y: i32) {
+    let style = MonoTextStyleBuilder::new()
+        .font(&embedded_graphics::mono_font::ascii::FONT_6X10)
+        .text_color(White)
+        .background_color(Black)
+        .build();
+
+    let text_style = TextStyleBuilder::new().baseline(Baseline::Top).build();
+
+    let _ = Text::with_text_style(text, Point::new(x, y), style, text_style).draw(display);
+}
+
+
+
+fn main() -> Result<(), std::io::Error> {
+        // Configure SPI
+    // Settings are taken from
+    let mut spi = Spidev::open("/dev/spidev0.0").expect("spidev directory");
+    let options = SpidevOptions::new()
+        .bits_per_word(8)
+        .max_speed_hz(4_000_000)
+        .mode(spidev::SpiModeFlags::SPI_MODE_0)
+        .build();
+    spi.configure(&options).expect("spi configuration");
+
+    // Configure Digital I/O Pin to be used as Chip Select for SPI
+    let cs = Pin::new(26); //BCM7 CE0
+    cs.export().expect("cs export");
+    while !cs.is_exported() {}
+    cs.set_direction(Direction::Out).expect("CS Direction");
+    cs.set_value(1).expect("CS Value set to 1");
+
+    let busy = Pin::new(24); // GPIO 24, board J-18
+    busy.export().expect("busy export");
+    while !busy.is_exported() {}
+    busy.set_direction(Direction::In).expect("busy Direction");
+    //busy.set_value(1).expect("busy Value set to 1");
+
+    let dc = Pin::new(25); // GPIO 25, board J-22
+    dc.export().expect("dc export");
+    while !dc.is_exported() {}
+    dc.set_direction(Direction::Out).expect("dc Direction");
+    dc.set_value(1).expect("dc Value set to 1");
+
+    let rst = Pin::new(17); // GPIO 17, board J-11
+    rst.export().expect("rst export");
+    while !rst.is_exported() {}
+    rst.set_direction(Direction::Out).expect("rst Direction");
+    rst.set_value(1).expect("rst Value set to 1");
+
+    let mut delay = Delay {};
+
+    let mut epd2in13 = Epd2in13::new(&mut spi, cs, busy, dc, rst, &mut delay).expect("eink initalize error");
+
+    // BDK example from website
+    let network = Network::Testnet; // Or this can be Network::Bitcoin, Network::Signet or Network::Regtest
+
+    // Generate fresh mnemonic
+    let mnemonic: GeneratedKey<_, miniscript::Segwitv0> = Mnemonic::generate((WordCount::Words12, Language::English)).unwrap();
+    // Convert mnemonic to string
+    let mnemonic_words = mnemonic.to_string();
+    // Parse a mnemonic
+    let mnemonic  = Mnemonic::parse(&mnemonic_words).unwrap();
+    // Generate the extended key
+    let xkey: ExtendedKey = mnemonic.into_extended_key().unwrap();
+    // Get xprv from the extended key
+    let xprv = xkey.into_xprv(network).unwrap();
+
+    // Create a BDK wallet structure using BIP 84 descriptor ("m/84h/1h/0h/0" and "m/84h/1h/0h/1")
+    let wallet = Wallet::new_offline(
+        Bip84(xprv, KeychainKind::External),
+        Some(Bip84(xprv, KeychainKind::Internal)),
+        network,
+        MemoryDatabase::default(),
+    )
+    .unwrap();
+
+    println!("mnemonic: {}\n\nrecv desc (pub key): {:#?}\n\nchng desc (pub key): {:#?}",
+        mnemonic_words,
+        wallet.get_descriptor_for_keychain(KeychainKind::External).to_string(),
+        wallet.get_descriptor_for_keychain(KeychainKind::Internal).to_string());
+
+    let mut display = Display2in13::default();
+    
+    let text_style = TextStyleBuilder::new().baseline(Baseline::Top).build();
+    let style = MonoTextStyleBuilder::new()
+        .font(&embedded_graphics::mono_font::ascii::FONT_10X20)
+        .text_color(White)
+        .background_color(Black)
+        .build();
+
+    let _ = Text::with_text_style("BDKPi", Point::new(10, 10), style, text_style).draw(&mut display);
+    let _ = Text::with_text_style(&mnemonic_words, Point::new(10, 30), style, text_style).draw(&mut display);
+
+    // Display updated frame
+    epd2in13.update_frame(&mut spi, display.buffer(), &mut delay)?;
+    epd2in13.display_frame(&mut spi, &mut delay).expect("display frame new graphics");
+
+    // Set the EPD to sleep
+    epd2in13.sleep(&mut spi, &mut delay)?;
+
+    Ok(())
+}
